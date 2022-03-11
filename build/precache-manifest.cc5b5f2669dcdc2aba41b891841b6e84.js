5ea382257ac4b45921ab9ebeb4ea0454d8f6c5d3
Ahmed Sajjad <uasajjad11198@outlook.com>
2022-03-11 17:00:34 -0500

    attempt to fix broken images

---
+++ build/precache-manifest.cc5b5f2669dcdc2aba41b891841b6e84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177e106cdc3b471b8f16bed68a99b447",
+    "url": "./index.html"
+  },
+  {
+    "revision": "893a3c65cb8b06fdb798",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "db37e72634dda052b9ad",
+    "url": "./static/css/main.6a6c9962.chunk.css"
+  },
+  {
+    "revision": "893a3c65cb8b06fdb798",
+    "url": "./static/js/2.3aa974b4.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.3aa974b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db37e72634dda052b9ad",
+    "url": "./static/js/main.c9359504.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
