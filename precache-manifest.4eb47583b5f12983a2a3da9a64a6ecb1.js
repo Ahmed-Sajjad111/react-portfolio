d637ea5bca1a6e6e5bf247f150a054fac5cf27a8
Ahmed Sajjad <uasajjad11198@outlook.com>
2022-03-14 14:13:10 -0400

    Updates

--- precache-manifest.4eb47583b5f12983a2a3da9a64a6ecb1.js
+++ precache-manifest.4eb47583b5f12983a2a3da9a64a6ecb1.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "177e106cdc3b471b8f16bed68a99b447",
+    "revision": "90267011bb45b6abc977ba5062758a84",
     "url": "./index.html"
   },
   {
-    "revision": "893a3c65cb8b06fdb798",
+    "revision": "9abe945b12297d5555d4",
     "url": "./static/css/2.d64471e2.chunk.css"
   },
   {
-    "revision": "db37e72634dda052b9ad",
+    "revision": "82f383065b4f51dca0c9",
     "url": "./static/css/main.6a6c9962.chunk.css"
   },
   {
-    "revision": "893a3c65cb8b06fdb798",
-    "url": "./static/js/2.3aa974b4.chunk.js"
+    "revision": "9abe945b12297d5555d4",
+    "url": "./static/js/2.291cac97.chunk.js"
   },
   {
     "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
-    "url": "./static/js/2.3aa974b4.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.291cac97.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "db37e72634dda052b9ad",
-    "url": "./static/js/main.c9359504.chunk.js"
+    "revision": "82f383065b4f51dca0c9",
+    "url": "./static/js/main.6adf221e.chunk.js"
   },
   {
     "revision": "1d6896b13d1316b5acb7",
